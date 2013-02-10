df76b823074400ffebcea4fc92a05c89f8871111
Jussi Kokkala <ussi Kokkala>
2013-02-10 23:39:14 +0200

    randomtext

---
+++ input/scripts/random_text.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+
+int main(int argc, char** argv) {
+    srand(time(0));
+    int alphabet_size=40;
+    int text_length=100;
+    if(argc>=2) text_length=atoi(argv[1]);
+    if(argc>=3) alphabet_size=atoi(argv[2]);
+    char low='!';
+    if(alphabet_size > 90) low=1;
+    for(int i=0;i<text_length;i++) cout<<(char)(( rand() % alphabet_size) + low);
+
+}
