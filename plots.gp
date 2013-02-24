939ee1ea332437f59ee946353f21241ec074aba1
Aleksi Hartikainen <aleksi.hartikainen@helsinki.fi>
2013-02-25 00:22:13 +0200

    Science: variable length patterns

--- plots.gp
+++ plots.gp
@@ -6,4 +6,10 @@ plot \
     "data/fixed_len.dat" using 1:2 title 'Aho-Corasick' with lines, \
     "data/fixed_len.dat" using 1:3 title 'Rabin-Karp' with lines;
 
+set terminal pdf
+set output 'var_len.pdf'
+set xlabel 'Total pattern length'
+plot \
+    "data/var_len.dat" using 1:2 title 'Aho-Corasick' with lines, \
+    "data/var_len.dat" using 1:3 title 'Rabin-Karp' with lines;
 
--- science.cpp
+++ science.cpp
@@ -14,48 +14,118 @@ using namespace std;
 
 string text;
 
-void fixed_len(FILE* fout)
+void fixed_len(const char* file)
 {
-    text = read_text("input/musketeers.txt");
     srand(0);
-    int count = 10000;
+    int count = 100;
+    int parts = 100;
+    int step = 3;
     int minlen = 6;
     int maxlen = 100;
-    vector<int> lens;
-    vector<string> patterns(count);
-    vector<int> cursor(count);
-    for (int i = 0; i < count; i++)
+    vector<double> times[3];
+    for (int i=0;i<3;i++)
+        times[i].resize(maxlen+1);
+
+    while(parts-->0)
     {
-        cursor[i] = rand()%(text.size()-maxlen-1);
-        for (int j = 0; j < minlen; j++)
+        cout << "parts = "<<parts<<"\n";
+        vector<string> patterns(count);
+        vector<int> cursor(count);
+        for (int i = 0; i < count; i++)
         {
-            patterns[i].push_back(text[cursor[i]++]);
+            cursor[i] = rand()%(text.size()-maxlen-1);
+            for (int j = 0; j < minlen; j++)
+            {
+                patterns[i].push_back(text[cursor[i]++]);
+            }
+        }
+        for (int len = minlen; len <= maxlen; len+=step)
+        {
+            cout << "len = "<<len<<"\n";
+            for (int i = 0; i < matcher_list_size; i++)
+            {
+                double t = timef();
+                match_vector matches;
+                matcher_list[i]->match(text, patterns, matches);
+            cout << matches.size() << " matches\n";
+                t = timef()-t;
+                times[i][len]+=t;
+                //fprintf(fout, "%8.8lf ", t);
+            }
+            // add one character to each pattern
+            for (int i = 0; i < count; i++)
+            {
+                patterns[i].push_back(text[cursor[i]++]);
+            }
         }
     }
-    for (int len = minlen; len <= maxlen; len++)
+    FILE* fout = fopen(file, "w");
+    for (int j = minlen; j <= maxlen; j+=step)
     {
-        fprintf(fout, "%8i ", len);
+        fprintf(fout, "%8i ", j);
+        for (int i = 0; i < matcher_list_size; i++)
+        {
+            fprintf(fout, "%8.8lf ", times[i][j]);;
+        }
+        fprintf(fout, "\n");
+    }
+    fclose(fout);
+}
+
+int minlen = 10;
+int maxlen = 1000;
+string rand_pattern()
+{
+    int len = minlen + rand()%(maxlen-minlen);
+    return text.substr(rand()%(text.size()-len),len);
+}
+
+void variable_len(const char* file)
+{
+    FILE* fout = fopen(file, "w");
+    srand(0);
+
+    int minlen = 100000;
+    int maxlen = 1000000;
+    int step = 100;;
+    vector<int> lens;
+    vector<string> patterns;
+    int total = 0;
+    while(total < minlen)
+    {
+        patterns.push_back(rand_pattern());
+        total+=patterns.back().size();
+    }
+
+    while(total <= maxlen)
+    {
+        fprintf(fout, "%8i ", total);
         for (int i = 0; i < matcher_list_size; i++)
         {
             double t = timef();
             match_vector matches;
             matcher_list[i]->match(text, patterns, matches);
+            cout << matches.size() << " matches\n";
             t = timef()-t;
             fprintf(fout, "%8.8lf ", t);
         }
         fprintf(fout, "\n");
-        // add one character to each pattern
-        for (int i = 0; i < count; i++)
+        for (int i = 0; i < step; i++)
         {
-            patterns[i].push_back(text[cursor[i]++]);
+            patterns.push_back(rand_pattern());
+            total+=patterns.back().size();
         }
+        // add one character to each pattern
     }
+    fclose(fout);
 }
 
-int main()
+int main(int argc, char** argv)
 {
-    FILE* o = fopen("data/fixed_len.dat","w");
-    fixed_len(o);
-    fclose(o);
+    text = read_text("input/short_musketeers.txt");
+    fixed_len("data/fixed_len.dat");
+
+    variable_len("data/var_len.dat");
+
     system("gnuplot plots.gp");
 }
