b333fac329b295f60ec98c55ed0c2e893d80ab01
Jussi Kokkala <yuizy@e42-37.cs.helsinki.fi>
2013-02-25 18:39:27 +0200

    splitposter

--- hyposter/split_poster.sh
+++ hyposter/split_poster.sh
@@ -1,4 +1,7 @@
-pslatex poster.tex
+pdflatex poster.tex
+pdflatex poster.tex
+pdf2ps poster.pdf
 tool/poster -mA4 -pA0 poster.ps > poster_A4.ps # UNCOMMENT
 ps2pdf poster_A4.ps
 rm poster_A4.ps
+rm poster.ps
