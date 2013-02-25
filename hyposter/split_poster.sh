23398d6a10a61055d76a5f7fb96629b6c483e550
Jussi Kokkala <yuizy@e42-37.cs.helsinki.fi>
2013-02-26 01:47:59 +0200

    evening

--- hyposter/split_poster.sh
+++ hyposter/split_poster.sh
@@ -1,7 +1,7 @@
-pdflatex poster.tex
-pdflatex poster.tex
-pdf2ps poster.pdf
-tool/poster -mA4 -pA0 poster.ps > poster_A4.ps # UNCOMMENT
-ps2pdf poster_A4.ps
-rm poster_A4.ps
-rm poster.ps
+pdflatex $1.tex
+pdflatex $1.tex
+pdf2ps $1.pdf
+tool/poster -mA4 -pA1 $1.ps > $1_A4.ps # UNCOMMENT
+ps2pdf $1_A4.ps
+rm $1_A4.ps
+rm $1.ps
